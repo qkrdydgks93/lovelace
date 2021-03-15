f8a9fe62ad5defd9df976b197421ffe09a03ded8
qkrdydgks93 <80001312+qkrdydgks93@users.noreply.github.com>
2021-03-15 22:43:21 +0900

    delete test files

--- test.js
+++
@@ -1,8 +0,0 @@
-let hello = document.getElementById("hello");
-
-const show = () => {
-    var content = document.getElementById("one");
-    one.style.display = "block";
-}
-
-hello.addEventListener('click',show);
